4565adf434b2a72051c3c8c3538678343210091d
PRIYASHREE ROY <101980665+Priyashree1999@users.noreply.github.com>
2022-03-28 12:46:56 -0500

    Add files via upload

---
+++ assignment3.3.py
@@ -0,0 +1,15 @@
+def counting_letters(a):
+    upper_case=0 
+    lower_case=0
+    for i in a:
+        if i.isupper():
+            upper_case+=1
+        elif i.islower():
+            lower_case+=1
+    print("No of upper case characters:",upper_case)  
+    print("No of lower case characters :",lower_case)
+    return   
+
+string ='The quick Brow Fox '
+l=counting_letters(string)
+
